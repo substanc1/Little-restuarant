9be9bd2fa28935fd0824fa58d7e210cdadf6e2ab
substanc1 <99292941+substanc1@users.noreply.github.com>
2023-10-06 13:40:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Home from "./components/home/Home";
+
+function App() {
+  return (
+    <>
+
+      <Nav />
+
+      <Home />
+
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
